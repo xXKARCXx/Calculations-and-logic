92dcc534c9553dc8ff74aa8d2a5bbfc5f1eb1f6d
xXKARCXx <aw3528231@gmail.com>
2025-02-09 18:27:53 +0000

    update 0.1
    I added a readme, cpp prop.json, launch.json and setting.json.
    I also update the converter menu by adding better visual structure making it easier to read and understand.
    one way i did this is by adding spacing and adding title and option names that make sense.

--- cal_time.cpp
+++ cal_time.cpp
@@ -11,16 +11,20 @@ void TimeConverter (){
 
 void converter_menu ()
 {
-    cout << "Option 1" << endl;
-    cout << "Option 2" << endl;
-    cout << "Exit" << endl;
+    cout << "                 1) Minutes to Seconds" << endl;
+    cout << endl;
+    cout << "                 2) Hours to seconds" << endl;
+    cout << endl;
+    cout << "                       3) Exit" << endl;
 }
 
 void min_secCon (){
     int sec = 60;
     int min ;
     cout << "Chose the amount of minutes you want to change to seconds: ";
+    cout << endl;
     cin >> min;
+    cout << endl;
     cout << "amout of seconds: " << (min * sec ) << endl;
 }
 
@@ -29,22 +33,29 @@ int sec;
 int minC = 3600;
 int hour;
 cout << "type the amount of hours you want to conveter to seconds: ";
+cout << endl;
 cin >> hour;
+cout << endl;
 cout << "This amount of secound in this amount of hours: " << (hour * minC) << endl;
 }
 
 int main(){
     int choice;
+    cout << "                                                   " << endl;
+    cout << "                 Choose Option 1 or 2 or 3" << endl;
+    cout<< "<------------------------------------------------------->" << endl;
     while (true)
     {
         converter_menu();
-
-        cout << "Enter Option 1 or 2 or 3" << endl;
-        cout<< "<------------------------------------------------------->" << endl;
+        cout << endl;
+        cout << "                Enter your choice: ";
         cin >> choice;
+        cout << endl;
+        cout << "              You have picked choice "<< choice << endl;
 
         if (choice == 3){
-            cout << "Exited. Goodbye Now!" << endl;
+            cout << endl;
+            cout << "                Exited. Goodbye Now!" << endl;
             cout << "                                              " << endl;
             break;
         }
